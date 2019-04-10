f69f6c639e8bce83a6ca0503a17e63d6ae36fe80
LGCoding <49463850+LGCoding@users.noreply.github.com>
2019-04-09 21:54:32 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include "Oct.h"
+#include "functionO.h"
+void test(function& fun) {
+	std::cout << "in\n";
+	fun(nullptr);
+}
+
+int main() {
+	funFunction(hi)
+		std::cout << "sss\n";
+	end(hi, me)
+	test(me);
+}

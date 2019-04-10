eaaaefa64dd6e364704139adb021c727ab33c8eb
LGCoding <49463850+LGCoding@users.noreply.github.com>
2019-04-09 21:55:07 -0400

    Create function.h

---
+++ function.h
@@ -0,0 +1,43 @@
+#pragma once
+
+struct link {
+	void* data;
+	link* next = nullptr;
+};
+
+struct linkList {
+	link* headLink;
+	int length = 0;
+	link* operator [] (int dataPoint) const {
+		link* hold = headLink;
+		for (int i = 0; i < dataPoint; i++) {
+			hold = hold->next;
+		}
+		return hold;
+	}
+	void add(void* data) {
+		length++;
+		link* dataL = new link;
+		dataL->data = data;
+		dataL->next = headLink;
+		headLink = dataL;
+	}
+};
+
+#define end(name, displayName) return nullptr;\
+ }};\
+name displayName;
+
+class function
+{
+public:
+	function() {};
+	virtual void* operator () (linkList* arrguments) { return nullptr; };
+};
+
+#define funFunction(name) \
+class name : public function\
+{\
+public:\
+	name() {};\
+	void* operator () (linkList* arrguments) {

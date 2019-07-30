0c177d1628ef15aa42fa9d4c71ae48692c43507d
LGCoding <49463850+LGCoding@users.noreply.github.com>
2019-07-30 11:45:55 -0400

    Update function.h

--- function.h
+++ function.h
@@ -1,48 +1,82 @@
 #pragma once
+#include <iostream>
+namespace Func {
+	struct link {
+		void* data;
+		link* next = nullptr;
+	};
 
-struct link {
-	void* data;
-	link* next = nullptr;
-};
+	struct LinkList {
+		link* headLink;
+		int length = 0;
 
-struct linkList {
-	link* headLink;
-	int length = 0;
-	link* operator [] (int dataPoint) const {
-		link* hold = headLink;
-		for (int i = 0; i < dataPoint; i++) {
-			hold = hold->next;
+		LinkList() {};
+		~LinkList() {
+			link* hold = headLink;
+			link* last;
+			for (int i = 0; i < length; i++) {
+				last = hold;
+				hold = hold->next;
+				delete last->data;
+				delete last;
+			}
+		};
+		template <typename... Args>
+		LinkList(Args... args) {
+			setup(args...);
+		};
+		template <typename T, typename... Args>
+		void setup(T first, Args... rest) {
+			T* temp = new T;
+			*temp = first;
+			add((void*)temp);
+			setup(rest...);
 		}
-		return hold;
-	}
-	void add(void* data) {
-		length++;
-		link* dataL = new link;
-		dataL->data = data;
-		dataL->next = headLink;
-		headLink = dataL;
-	}
-};
+		template <typename T>
+		void setup(T first) {
+			T* temp = new T;
+			*temp = first;
+			add((void*)temp);
+		}
 
-#define end(name, displayName) return nullptr;\
- }};\
-name displayName;
+		void* get(int dataPoint) const {
+			if (dataPoint >= length) {
+				throw "out of range Linked list";
+			}
+			link* hold = headLink;
+			for (int i = 0; i < dataPoint; i++) {
+				hold = hold->next;
+			}
+			return hold->data;
+		}
+		void add(void* data) {
+			length++;
+			link* dataL = new link;
+			dataL->data = data;
+			dataL->next = headLink;
+			headLink = dataL;
+		}
+	};
 
-class function
-{
-public:
-	function() {};
-	virtual void* operator () (linkList* arrguments = nullptr) { return nullptr; }
-};
+	#define Func_arg(place,type) *((type*)(args->get(place)))
+	////////////////////////////////////////////////////////////////////////////////////////////
 
-#define Function(name, code) \
-\
-class name : public function\
-{\
-public:\
-	name() {};\
-	void* operator () (linkList* arrguments = nullptr) {code return nullptr;}\
-};\
-name name;
+	class FUNCTION
+	{
+	public:
+		FUNCTION() {};
+		virtual void* operator () (LinkList* args = nullptr) { return nullptr; }
+	};
 
+	////////////////////////////////////////////////////////////////////////////////////////////
+	#define Func_Function(name) \
+	class name : public Func::FUNCTION\
+	{\
+	public:\
+		name() {};\
+		void* operator () (Func::LinkList* args = nullptr) { \
 
+	#define Func_end(name) \
+	 return nullptr;\
+	 }}name;
+}
